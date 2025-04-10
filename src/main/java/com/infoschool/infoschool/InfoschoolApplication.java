d9c33f03364accee6ef9dd6878bd99a4d486594b
Fabio Sarcinelli <fabiosarci125@gmail.com>
2025-04-10 15:47:04 +0200

    inizializzazione progetto java

---
+++ src/main/java/com/infoschool/infoschool/InfoschoolApplication.java
@@ -0,0 +1,13 @@
+package com.infoschool.infoschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfoschoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfoschoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infoschool/infoschool/InfoschoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infoschool.infoschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfoschoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
