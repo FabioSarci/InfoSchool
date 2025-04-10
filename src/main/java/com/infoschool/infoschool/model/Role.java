de50f812397c451350e4ede1531e9c9f88a9b52d
Fabio Sarcinelli <fabiosarci125@gmail.com>
2025-04-10 17:12:46 +0200

    creazione dei model Role e User, dell' enum ERole per poter integrare subito spring security

---
+++ src/main/java/com/infoschool/infoschool/model/Role.java
@@ -0,0 +1,30 @@
+package com.infoschool.infoschool.model;
+
+import com.infoschool.infoschool.util.ERole;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "roles")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Role {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Enumerated(EnumType.STRING)
+  @Column(length = 20)
+  private ERole name;
+}
---
+++ src/main/java/com/infoschool/infoschool/util/ERole.java
@@ -0,0 +1,8 @@
+package com.infoschool.infoschool.util;
+
+public enum ERole {
+    ROLE_USER,
+    ROLE_TEACHER,
+    ROLE_ADMIN
+  }
+  
